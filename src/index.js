c97f429c18d81006f7fd9290f9a52bd08c37ed08
Tushar Mangla <tushar.mangla1120@gmail.com>
2021-07-22 21:01:27 +0530

    change

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const taskRouter = require('./routers/tasks')
 
 const app = express()
 const port = process.env.PORT
